ca7b215b20a6640f53ba24742432d8e5ba01620a
Kotikov <11deadsluts@gmail.com>
2022-11-13 16:22:11 +0300

    the first task is solved

--- homework.cs
+++ homework.cs
@@ -4,15 +4,21 @@
 // 0, 0 - >1
 // 2, 4 -> 16
 
- 
+// int NumberToThePower(int x, int n)
+// {
+//     int power = 1;
+//     for (int i = 0; i < n; i++)
+//     {
+//         power *= x;
+//     }
+//     return power;
+// }
 
-
-
-
-
-
-
-
+// System.Console.WriteLine("Pls, Input the integer number you want to raise to a power:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input the integer power:");
+// int power = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("your result is " + NumberToThePower(num,power)+", Soooo....What will you do with that information?"); 
 
 
 
