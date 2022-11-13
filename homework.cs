1197f17a0572b9ceeeb8c54ebc165b2f0ef5bc2f
Kotikov <11deadsluts@gmail.com>
2022-11-13 18:14:36 +0300

    the second task is solved

--- homework.cs
+++ homework.cs
@@ -18,17 +18,29 @@
 // int num = Convert.ToInt32(Console.ReadLine());
 // System.Console.WriteLine("Input the integer power:");
 // int power = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("your result is " + NumberToThePower(num,power)+", Soooo....What will you do with that information?"); 
-
-
+// System.Console.WriteLine("your result is " + NumberToThePower(num,power)+", aaaaand....What will you do with that information?"); 
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
 // 452 -> 11
-
 // 82 -> 10
-
 // 9012 -> 12
+
+int CountOfDigits(int num)
+{
+    int sum = 0;
+    int temp;
+    while (num != 0)
+    {
+        temp = num%10;
+        sum += temp;
+        num = num / 10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine($"Input integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"the sum of the numbers in the \"{num}\" is {CountOfDigits(num)}, Sooooo...another math from me, i'm not just a computer, i'm a calculator :'( ");
 
 // Задача 29: Напишите программу, которая создает массив заданного пользователем размера, 
 //заполняет массив элементами от 1 до 99 и выводит их на экран.
